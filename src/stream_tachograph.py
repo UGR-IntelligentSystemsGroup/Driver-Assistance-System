87017aaaf26d37819579e8700aa61d36e9d42639
Ignacio Vellido <ignaciove@correo.ugr.es>
2021-11-26 13:19:48 +0100

    Streaming script now writing header

--- src/stream_tachograph.py
+++ src/stream_tachograph.py
@@ -17,7 +17,10 @@ OUT_PATH = "tmp/{}.csv".format(DRIVER_ID)
 
 df = pd.read_csv(DATA_PATH)
 
-for i in range(len(df)):
+# Write header
+df.loc[[0]].to_csv(OUT_PATH, mode='a', header=True, index=False)
+
+for i in range(1, len(df)):
     df.loc[[i]].to_csv(OUT_PATH, mode='a', header=False, index=False)
 
     sleep(random.uniform(10,30))
